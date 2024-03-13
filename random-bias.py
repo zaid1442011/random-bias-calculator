cbc71b01c553b91e6855e1830c78cbe789936b5f
Zaid <zaid1442011@icloud.com>
2024-03-13 16:41:12 +0300

    Initial commit

---
+++ random-bias.py
@@ -0,0 +1,17 @@
+import random
+import os
+
+maxrand = 9007199254740991
+EVEN = 0
+ODD = 0
+tries = 0
+
+while True:
+   if ((random.randint(1, maxrand)) % 2) == 0:
+      EVEN = EVEN + 1
+   else: 
+      ODD = ODD + 1
+   tries = tries + 1
+
+   os.system('clear')
+   print("Odd:", ODD, "Even:", EVEN, "Odd %:", ((ODD / tries) * 100), "Even %:", ((EVEN / tries) * 100))
